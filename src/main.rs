a6cdca4c0692dbecbfddd560e06d449635d107f0
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-10-12 14:18:37 +1100

    feat: Basic GET request and JSON parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
-use std::env;
+#![allow(non_snake_case)]
 
-fn main() {
-    let AUD_TO_USD = 0.729746;
+use std::env;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct RatesModel {
+    EUR: f32,
+    USD: f32,
+    GBP: f32,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseData {
+    rates: RatesModel,
+    base: String,
+    date: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,GBP,EUR")
+        .await?
+        .json::<ResponseData>()
+        .await?;
+
+    println!("{:#?}", resp);
+
+    let aud_to_usd_rate = resp.rates.USD;
 
     let args: Vec<String> = env::args().collect();
 
@@ -9,8 +34,9 @@ fn main() {
     let input_currency = &args[2];
     let output_currency = &args[3];
     let mut output_amount = args[1].parse::<f32>().unwrap();
-    output_amount = output_amount / AUD_TO_USD;
+    output_amount = output_amount * aud_to_usd_rate;
 
-    println!("Converting {} {}", input_currency, input_amount);
-    println!("Which is also {} {}", output_currency, output_amount);
+    println!("{} {} is {} {}", input_currency, input_amount, output_currency, output_amount);
+
+    return Ok(());
 }
