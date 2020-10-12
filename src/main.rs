a157841e0e79a9717fb7c4168582b705eb1a27bc
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-10-12 14:47:29 +1100

    feat: Use provided arguments for URL generation

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,20 @@ struct ResponseData {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,GBP,EUR")
-        .await?
-        .json::<ResponseData>()
-        .await?;
+    let args: Vec<String> = env::args().collect();
+    let input_amount = &args[1];
+    let input_currency = &args[2];
+    let output_currency = &args[3];
+    let url = format!("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,EUR,GBP");
+    // let url = format!("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,EUR", input_currency, output_currency);
+
+    let resp = reqwest::get(&url).await?.json::<ResponseData>().await?;
 
     println!("{:#?}", resp);
 
     let aud_to_usd_rate = resp.rates.USD;
 
-    let args: Vec<String> = env::args().collect();
-
-    let input_amount = &args[1];
-    let input_currency = &args[2];
-    let output_currency = &args[3];
-    let mut output_amount = args[1].parse::<f32>().unwrap();
+    let mut output_amount = input_amount.parse::<f32>().unwrap();
     output_amount = output_amount * aud_to_usd_rate;
 
     println!(
