bdd495a8e5485fc3a84315ac4cad1e53f1c36752
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-10-24 14:27:09 +1100

    feat: Make it actually utilize inputs
    - Remove hardcoded AUD/USD
    - Make it support any 2 input currencies
    - Clean up formatting

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ```bash
 cargo run --release 100 aud usd
 cargo run 123.45 aud usd
+cargo run 499 usd aud
 ccon 100 aud usd
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #![allow(non_snake_case)]
 
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
 
 #[derive(Deserialize, Debug)]
-struct RatesModel {
-    EUR: f32,
-    USD: f32,
-    GBP: f32,
-}
-
-#[derive(Deserialize, Debug)]
 struct ResponseData {
-    rates: RatesModel,
+    rates: HashMap<String, f32>,
     base: String,
     date: String,
 }
@@ -21,22 +15,29 @@ struct ResponseData {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let input_amount = &args[1];
-    let input_currency = &args[2];
-    let output_currency = &args[3];
-    let url = format!("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,EUR,GBP");
-    // let url = format!("https://api.exchangeratesapi.io/latest?base=AUD&symbols=USD,EUR", input_currency, output_currency);
+    let input_currency = &args[2].to_uppercase();
+    let output_currency = &args[3].to_uppercase();
 
+    let url = format!(
+        "https://api.exchangeratesapi.io/latest?base={}&symbols={}",
+        input_currency, output_currency
+    );
+    // println!("{:#?}", url);
+    // TODO: Error handling if not 200
+    println!("Fetching conversion rates...");
     let resp = reqwest::get(&url).await?.json::<ResponseData>().await?;
+    // println!("{:#?}", resp);
 
-    println!("{:#?}", resp);
+    // QUESTION: I still don't understand * and &
+    // TODO: error handling
+    let conversion_rate = *resp.rates.get(output_currency).unwrap();
+    println!("Conversion rate is: {}", conversion_rate);
 
-    let aud_to_usd_rate = resp.rates.USD;
-
-    let mut output_amount = input_amount.parse::<f32>().unwrap();
-    output_amount = output_amount * aud_to_usd_rate;
+    let parsed_input = input_amount.parse::<f32>().unwrap();
+    let output_amount = parsed_input * conversion_rate;
 
     println!(
-        "{} {} is {} {}",
+        "{}{} is {}{}",
         input_currency, input_amount, output_currency, output_amount
     );
 
